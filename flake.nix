6d147acee64e469b2d3b5029196f5b92ea58736b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-28 17:37:38 +0100

    Initial firmware commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Keyboard";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    flake-utils.url = github:numtide/flake-utils;
+    rust-overlay = {
+      url = github:oxalica/rust-overlay;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay } @ args: flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in
+    {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          llvmPackages_latest.bintools
+          picotool
+          elf2uf2-rs
+          probe-run
+          flip-link
+          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" ];
+            targets = [ "thumbv6m-none-eabi" ];
+          }))
+        ];
+      };
+    }
+  );
+}
